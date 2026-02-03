f42242f28ccc8c1f3503b76eb9a2b66b11617922
Bryant Jopplin <35749471+StealUrKill@users.noreply.github.com>
2026-02-03 17:44:06 -0600

    Updated to use better with IRM|IEX - Also applies to stock rustdesk config if required.

--- custom_config_generator.ps1
+++ custom_config_generator.ps1
@@ -1,17 +1,69 @@
-$json = '{"host":"yourdomain.com","key":"yourkey","api":"yourapi"}'
+$rdhost = Read-Host "Enter RustDesk host (example: yourdomain.com)"
+Write-Host ""
+$rdkey  = Read-Host "Enter RustDesk key"
+Write-Host ""
+$rdapi  = Read-Host "Enter RustDesk API (example: https://yourdomain.com)"
 
+
+$json = '{"host":"' + $rdhost + '","key":"' + $rdkey + '","api":"' + $rdapi + '"}'
 $b64 = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($json))
 $chars = $b64.ToCharArray()
 [Array]::Reverse($chars)
 $code = -join $chars
 
-# generate qr code for mobile
-$qrText  = "config=$json"
-$desktop = [Environment]::GetFolderPath("Desktop")
-$outFile = Join-Path $desktop "rustdesk_config_qr.png"
-$url = "https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=$([uri]::EscapeDataString($qrText))"
-Invoke-WebRequest -Uri $url -OutFile $outFile
 
-Write-Host "QR saved to: $outFile"
-Write-Host "QR content: $qrText"
-Write-Host "Config String: $code"
+Write-Host ""
+$genQR = Read-Host "Generate QR? (y/n)"
+
+
+if ($genQR -match '^(y|yes)$') {
+
+	$qrText = "config=$json"
+	$desktop = [Environment]::GetFolderPath("Desktop")
+	$outFile = Join-Path $desktop "rustdesk_config_qr.png"
+
+	$url = "https://api.qrserver.com/v1/create-qr-code/?size=240x240&margin=2&data=$([uri]::EscapeDataString($qrText))"
+	Invoke-WebRequest -Uri $url -OutFile $outFile
+
+	Write-Host ""
+	Write-Host "QR saved to: $outFile"
+	Write-Host ""
+	Write-Host "QR content: $qrText"
+}
+
+
+Write-Host ""
+Write-Host "Config String:"
+Write-Host ""
+$code | Set-Clipboard
+Write-Host $code
+Write-Host ""
+Write-Host "(Config copied to clipboard)"
+Write-Host ""
+$apply = Read-Host "Apply this config to Rustdesk now? (y/n)"
+
+
+if ($apply -match '^(y|yes)$') {
+	$rdExe = "C:\Program Files\RustDesk\rustdesk.exe"
+
+	if (Test-Path $rdExe) {
+		Write-Host "Applying config to Rustdesk..."
+		& $rdExe --config $code
+	}
+	else {
+		Write-Host ""
+		Write-Host "Rustdesk not found at:"
+		Write-Host $rdExe
+		Write-Host ""
+
+		$customExe = Read-Host "Enter the full path to the Rustdesk executable or press Enter to cancel"
+
+		if ($customExe -and (Test-Path $customExe)) {
+			Write-Host "Applying config using custom executable for pro users"
+			& $customExe --config $code
+		}
+		else {
+			Write-Host "No valid executable provided. Skipping apply."
+		}
+	}
+}
